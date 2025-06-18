584ed3e0dd757bc0c6c70452afb5863e69a6691f
David Baldassin <david.baldassin@dolby.com>
2025-06-18 13:56:26 +0200

    add wattsup metrics and log in csv

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,14 @@ use std::time::Instant;
 
 use sysinfo::{System};
 
+use std::ffi::CString;
+
+use csv::Writer;
+use serde::Serialize;
+
+// use cgroups_proactive_reclaim;
+// use cgroups_proactive_reclaim::CgroupsReclaimManager;
+
 const ROOT_XML_PATH: &str = "/home/tobias/Documents/medooze-vm-monitor/root.xml";
 const ROOT_IMG_PATH: &str = "/var/lib/libvirt/images/medooze.qcow2";
 const VM_XML_PATH: &str = "/tmp/vm-xml";
@@ -20,9 +28,104 @@ const VM_IMG_PATH: &str = "/tmp/vm-img";
 const VM_INITIAL_MEMORY: u64 = 4 * 1024 * 1024 * 1024; // 4 GB
 const VM_SAFETY_MEMORY: u64 = 2 * 1024 * 1024 * 1024; // 1 GB
 
+#[link(name = "wattsup")]
+unsafe extern "C" {
+    fn wu_get_data(fd: i32, arr: *mut f64) -> i32;
+    // fn wu_get_num_metric() -> i32;
+    //int open_device(char * device_name, int * dev_fd)
+    fn open_device(device_name: *const i8, dev_fd: *mut i32) -> i32;
+    // int setup_serial_device(int dev_fd)
+    fn setup_serial_device(dev_fd: i32) -> i32;
+    fn wu_close(dev_fd: i32);
+    fn wu_clear(dev_fd: i32) -> i32;
+}
+
 struct XmlConfig {
     vm_name: String,
     disk_img: String, // in MB
+}
+
+#[derive(serde::Serialize, Default)]
+struct LogEntry {
+    timestamp: u64,
+    cpu_usage: f64,
+    memory_usage: u64, // in KB
+    num_vm: u32,
+    watts: f64,
+    volts: f64,
+    amps: f64,
+    kwh: f64,
+    cost: f64,
+    mo_kwh: f64,
+    mo_cost: f64,
+    max_watts: f64,
+    min_volts: f64,
+    max_amps: f64,
+    min_watts: f64,
+    max_volts: f64,
+    min_amps: f64,
+    power_factor: f64,
+    duty_cycle: f64,
+    power_cycle: f64,
+    frequency: f64,
+    va: f64,
+}
+
+#[repr(u8)]
+enum WattsupMetric {
+    Watts = 0,
+    Volts,
+    Amps,
+    Kwh,
+    Cost,
+    MoKwh,
+    MoCost,
+    MaxWatts,
+    MinVolts,
+    MaxAmps,
+    MinWatts,
+    MaxVolts,
+    MinAmps,
+    PowerFactor,
+    DutyCycle,
+    PowerCycle,
+    Frequency,
+    Va,
+}
+
+const WATTSUP_NUM_METRICS: usize = 18;
+
+fn create_csv_writer(path: &str) -> Writer<std::fs::File> {
+    let file = std::fs::File::create(path).expect("Failed to create CSV file");
+    let mut wtr = Writer::from_writer(file);
+
+    wtr
+}
+
+fn write_log_entry(wtr: &mut Writer<std::fs::File>, logentry: &LogEntry) {
+    wtr.serialize(logentry).expect("Failed to write log entry to CSV");
+    wtr.flush().expect("Failed to flush CSV writer");
+}
+
+fn add_wattsup_metrics(logentry: &mut LogEntry, wattsup_data: &[f64; WATTSUP_NUM_METRICS]) {
+    logentry.watts = wattsup_data[WattsupMetric::Watts as usize];
+    logentry.volts = wattsup_data[WattsupMetric::Volts as usize];
+    logentry.amps = wattsup_data[WattsupMetric::Amps as usize];
+    logentry.kwh = wattsup_data[WattsupMetric::Kwh as usize];
+    logentry.cost = wattsup_data[WattsupMetric::Cost as usize];
+    logentry.mo_kwh = wattsup_data[WattsupMetric::MoKwh as usize];
+    logentry.mo_cost = wattsup_data[WattsupMetric::MoCost as usize];
+    logentry.max_watts = wattsup_data[WattsupMetric::MaxWatts as usize];
+    logentry.min_volts = wattsup_data[WattsupMetric::MinVolts as usize];
+    logentry.max_amps = wattsup_data[WattsupMetric::MaxAmps as usize];
+    logentry.min_watts = wattsup_data[WattsupMetric::MinWatts as usize];
+    logentry.max_volts = wattsup_data[WattsupMetric::MaxVolts as usize];
+    logentry.min_amps = wattsup_data[WattsupMetric::MinAmps as usize];
+    logentry.power_factor = wattsup_data[WattsupMetric::PowerFactor as usize];
+    logentry.duty_cycle = wattsup_data[WattsupMetric::DutyCycle as usize];
+    logentry.power_cycle = wattsup_data[WattsupMetric::PowerCycle as usize];
+    logentry.frequency = wattsup_data[WattsupMetric::Frequency as usize];
+    logentry.va = wattsup_data[WattsupMetric::Va as usize].into();
 }
 
 fn create_xml_from_template(config: &XmlConfig) -> String {
@@ -93,6 +196,29 @@ fn get_vm_ipv4(domain: &Domain) -> String {
     String::new() // return empty string if no IPv4 address found
 }
 
+fn init_wattsup() -> i32 {
+    let mut dev_fd: i32 = 0;
+    let device_name = CString::new("/dev/ttyUSB0").expect("CString::new failed");
+    
+    unsafe {
+        if open_device(device_name.as_ptr(), &mut dev_fd) != 0 {
+            println!("Failed to open WattsUp device");
+            return -1;
+        }
+        if setup_serial_device(dev_fd) != 0 {
+            println!("Failed to setup WattsUp device");
+            wu_close(dev_fd);
+            return -1;
+        }
+
+        println!("WattsUp device opened successfully: {}", dev_fd);
+        wu_clear(dev_fd);
+    }
+    
+    println!("WattsUp device cleared successfully");
+    dev_fd
+}
+
 fn main() {
     let mut sys = System::new_all();
     let mut domains = Vec::new();
@@ -102,6 +228,17 @@ fn main() {
     // domain.create().expect("Failed to start domain");
 
     // println!("Pwd : {}", std::env::current_dir().unwrap().display());
+    println!("Current working directory: {}", std::env::current_dir().unwrap().display());
+
+    let wattsup = init_wattsup();
+    println!("WattsUp device file descriptor: {}", wattsup);
+    if wattsup < 0 {
+        std::process::exit(1);
+    }
+
+    println!("WattsUp device initialized successfully");
+    
+    let mut wattsup_data:[f64; WATTSUP_NUM_METRICS] = [0.; WATTSUP_NUM_METRICS];
 
     let mut num_vm = 0; 
 
@@ -115,13 +252,19 @@ fn main() {
 
     let initial_free_memory = fetch_free_memory(&mut sys);
     let start = Instant::now();
+    let mut time : u64 = 0;
+
+    const WAIT_TIME : u64 = 10; // seconds
+    let mut time_since_vm_creation = 0;
 
     std::fs::create_dir_all(VM_IMG_PATH).expect(format!("Failed to create {} directory", VM_IMG_PATH).as_str());
+
+    let mut wtr = create_csv_writer("vm_autoscaler_log.csv");
 
     while running.load(Ordering::SeqCst) {
         let free_memory = fetch_free_memory(&mut sys);
 
-        if free_memory > VM_INITIAL_MEMORY + VM_SAFETY_MEMORY {
+        if time_since_vm_creation >= WAIT_TIME && free_memory > VM_INITIAL_MEMORY + VM_SAFETY_MEMORY {
             let config = XmlConfig {
                 vm_name: format!("sfu-{}", num_vm),
                 disk_img: format!("{}/sfu-{}.qcow2", VM_IMG_PATH, num_vm), // 1 GB
@@ -135,15 +278,40 @@ fn main() {
             domain.create().expect("Failed to start new VM");
             num_vm += 1;
             
-            std::thread::sleep(std::time::Duration::from_secs(60));
+            // std::thread::sleep(std::time::Duration::from_secs(60));
             
-            let ipv4 = get_vm_ipv4(&domain);
+            // let ipv4 = get_vm_ipv4(&domain);
             domains.push(domain);
-            println!("VM {} created with IPv4: {}", config.vm_name, ipv4);
-
+            // println!("VM {} created with IPv4: {}", config.vm_name, ipv4);
+            time_since_vm_creation = 0;
         } else {
-            std::thread::sleep(std::time::Duration::from_secs(1));
+            // std::thread::sleep(std::time::Duration::from_secs(1));
+            time_since_vm_creation += 1;
         }
+
+        unsafe {
+            wu_get_data(wattsup, wattsup_data.as_mut_ptr());
+        }
+
+        // println!("WattsUp Data: {:?}", wattsup_data);
+        let mut logentry = LogEntry {
+            timestamp: start.elapsed().as_secs(),
+            cpu_usage: sys.global_cpu_usage() as f64,
+            memory_usage: sys.used_memory() / (1024 * 1024), // convert to MB
+            num_vm: num_vm as u32,
+            ..LogEntry::default()
+        };
+
+        add_wattsup_metrics(&mut logentry, &wattsup_data);
+
+        write_log_entry(&mut wtr, &logentry);
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        time += 1;
+    }
+
+    unsafe {
+        wu_close(wattsup);
     }
 
     let elapsed = start.elapsed();
